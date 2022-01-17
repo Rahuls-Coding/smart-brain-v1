e34c706be48e62d1f9cdc31d6cea2cd4ba52e3fb
Rahul <rahulscodingworld@gmail.com>
2022-01-17 15:54:52 -0500

    fixed box position

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -6,7 +6,7 @@ const FaceRecognition = ({imageUrl, box}) => {
         return (
         <div className="center ma" >
             <div className="absolute mt2">
-                <img id= "inputimage" className="br4 ba bw2"src={imageUrl} alt="image" width='400px' height='auto' />
+                <img id= "inputimage" className="br4 ba bw2"src={imageUrl} alt="Image Not Found!" width='400px' height='auto' />
                 <div className="bounding-box" style = {{top:box.topRow , right:box.rightCol , bottom: box.bottomRow, left: box.leftCol}}></div>
             </div>
             
