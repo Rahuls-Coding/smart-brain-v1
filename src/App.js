f440152607c310b8c58f1ac88927e0e9ab3ef3b6
Rahul <rahulscodingworld@gmail.com>
2022-03-28 21:43:05 -0400

    added registration

--- src/App.js
+++ src/App.js
@@ -27,10 +27,25 @@ class App extends Component {
       box: {},
       route: "signin",
       isSignedIn: false,
+      user: {
+            id: "",
+            name: "",
+            email: "",
+            entries: 0,
+            joined: "",
+      },
     }
   }
 
-  
+  loadUser = (data) => {
+    this.setState({ user: {
+            id: data.id,
+            name: data.name,
+            email: data.email,
+            entries: data.entries,
+            joined: data.joined,
+    }})
+  }
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById("inputimage");
@@ -96,8 +111,8 @@ class App extends Component {
       
       : (
         route === "signin" 
-        ? <Signin onRouteChange={this.onRouteChange}/> 
-        : <Register onRouteChange={this.onRouteChange}/>
+        ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/> 
+        : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
       )
       }
     </div>
